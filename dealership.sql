15256e0d67e5e9bffc9ef1685285bc023f8b4803
Sharmain <sharmainhenderson@Sharmains-MacBook-Air.local>
2023-08-24 14:39:50 -0700

    altered table data and column names; insert data with a stored function

--- dealership.sql
+++ dealership.sql
@@ -65,24 +65,24 @@ CREATE TABLE service_ticket (
   ticket_id SERIAL,
   need_parts BOOL,
   name_of_service VARCHAR(100),
-  customer_id INTEGER,
-  FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+  customer_no INTEGER,
+  FOREIGN KEY (customer_no) REFERENCES customer(customer_no)
 );
 
 --Service table for dealership service department
 CREATE TABLE service (
    ticket_id INTEGER,
    car_serial INTEGER,
-   staff_id INTEGER,
-   customer_id INTEGER,
-   FOREIGN KEY (staff_id) REFERENCES mechanic(staff_id),
-   FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+   staff_no INTEGER,
+   customer_no INTEGER,
+   FOREIGN KEY (staff_no) REFERENCES mechanic(staff_no),
+   FOREIGN KEY (customer_no) REFERENCES customer(customer_no),
    FOREIGN KEY (car_serial) REFERENCES service_ticket(car_serial)
 );
 
 
 --Insert into salesperson
-INSERT INTO salesperson(salesperson_id, first_name,  last_name)
+INSERT INTO salesperson(salesperson_no, first_name,  last_name)
 VALUES
      ('1','Bonnie', 'Samuels'),
      ('2', 'Frank', 'White'),
@@ -91,7 +91,7 @@ VALUES
  
 
 --Insert into customer
-INSERT INTO customer(customer_id, first_name,  last_name, address, phone_number, email)
+INSERT INTO customer(customer_no, first_name,  last_name, address, phone_number, email)
 VALUES
      ('1','Manuel', 'Adams', '833 E. Thorne Drive Port Washington, NY 11050', '(717) 550-1675','andreashenemann@discslot.com'),
      ('2', 'Pluto', 'Ese', '69 Marlborough St. De Pere, WI 54115', '(206) 342-8631)', 'veratelyukova@earthxqe.com'),
@@ -100,7 +100,7 @@ VALUES
 	
 
 --Insert into inventory
-INSERT INTO inventory(car_id, make, model, color, _year, car_for_sale)
+INSERT INTO inventory(car_no, make, model, color, _year, car_for_sale)
 VALUES
 	('1', 'Audi', 'R8', 'Yellow', '2023', 'yes'),
 	('2', 'BMW', 'X5', 'Gun Metal', '2024', 'n'),
@@ -108,65 +108,75 @@ VALUES
 
 
 --Insert into invoice
-INSERT INTO invoice(invoice_id, invoice_date, amount, salesperson_id, customer_id, car_id)
+INSERT INTO invoice(invoice_no, invoice_date, amount, salesperson_no, customer_no, car_no)
 VALUES
 	('33', '2023-08-22', '490.00', '2', '1', '3'),
 	('34', '2023-08-22', '600.00', '2', '2', '1');
 
 
 --Insert into sales
-INSERT INTO sales(customer_id, car_id, salesperson_id)
+INSERT INTO sales(customer_no, car_no, salesperson_no)
 VALUES
 	('1','3','2'),
 	('2','1','2');
 
 
 --Insert into service ticket
-INSERT INTO service_ticket(car_serial, ticket_id, need_parts, name_of_service, customer_id)
+INSERT INTO service_ticket(car_serial, ticket_id, need_parts, name_of_service, customer_no)
 VALUES
 	('1049', '1', 'false', 'oil change', '3'),
 	('1050', '2', 'true', 'timing belt replacement', '4');
 
 
 --Insert into mechanic
-INSERT INTO mechanic(staff_id, first_name, last_name, car_serial)
+INSERT INTO mechanic(staff_no, first_name, last_name, car_serial)
 VALUES
 	('10', 'Fran', 'Veronika', '1049'),
 	('11', 'Lleu', 'Yash', '1050');
 
 
 --Insert into service
-INSERT INTO service(ticket_id, car_serial, staff_id, customer_id)
+INSERT INTO service(ticket_id, car_serial, staff_no, customer_no)
 VALUES
 	('1', '1049', '10', '3'),
 	('2', '1050', '11', '4');
 
 
 -- Stored Function to insert data into the invoice table
-CREATE FUNCTION add_invoice(invoice_id INTEGER, invoice_date DATE, amount INTEGER, salesperson_id INTEGER, customer_id INTEGER, car_id INTEGER)
+CREATE FUNCTION add_invoice(invoice_no INTEGER, invoice_date DATE, 
+amount INTEGER, salesperson_no INTEGER, customer_no INTEGER, car_no INTEGER)
 RETURNS void
 AS $MAIN$
 BEGIN
-	INSERT INTO invoice(invoice_id, invoice_date, amount, salesperson_id, customer_id, car_id)
-	VALUES(_invoice_id, _invoice_date, _amount, _salesperson_id, _customer_id, _car_id);
+	INSERT INTO invoice(invoice_no, invoice_date, amount, salesperson_no, customer_no, car_no)
+	VALUES(_invoice_no, _invoice_date, _amount, _salesperson_no, _customer_no, _car_no);
 END;
 $MAIN$
 LANGUAGE plpgsql;
+
 
 -- Stored Function to insert data into the service ticket table
-CREATE FUNCTION add_service_ticket(car_serial INTEGER, ticket_id INTEGER, need_parts BOOL, name_of_service VARCHAR, customer_id INTEGER)
+CREATE FUNCTION add_service_ticket(car_serial INTEGER, ticket_id INTEGER, 
+need_parts BOOL, name_of_service VARCHAR, customer_no INTEGER)
 RETURNS void
 AS $MAIN$
 BEGIN
-	INSERT INTO service_ticket(car_serial, ticket_id, need_parts, name_of_service, customer_id)
-	VALUES(_car_serial, _ticket_id, _need_parts, _name_of_service, _customer_id);
+	INSERT INTO service_ticket(car_serial, ticket_id, need_parts, name_of_service, customer_no)
+	VALUES(_car_serial, _ticket_id, _need_parts, _name_of_service, _customer_no);
 END;
 $MAIN$
 LANGUAGE plpgsql;
 
+-- Call function
+SELECT add_service_ticket('35', '12', 'yes', 'transmission', '2')
+SELECT add_service_ticket('36', '12', 'yes', 'tires', '3');
+
+-- Verify addition
+SELECT * FROM service_ticket
 
 --Change column name in mechanic table
 ALTER TABLE mechanic
 RENAME COLUMN car_id to car_serial;
+
 
  
